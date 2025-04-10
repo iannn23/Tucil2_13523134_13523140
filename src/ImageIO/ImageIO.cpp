fb7bfdb20006de393305539f59950c9acbeb0b3b
Mahesa Fadhillah Andre <167867383+mahesa005@users.noreply.github.com>
2025-04-11 00:31:36 +0700

    feat: finished ImageIO

--- src/ImageIO/ImageIO.cpp
+++ src/ImageIO/ImageIO.cpp
@@ -3,7 +3,9 @@
 #include <string>
 
 #define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image.h"
+#include "stb_image_write.h"
 
 using namespace std;
 
@@ -31,4 +33,25 @@ vector<vector<vector<unsigned char>>> loadImage(const string& path, int& width, 
     
     stbi_image_free(data);
     return rgb;
+}
+
+void saveImage(const string&path, const vector<vector<vector<unsigned char>>>& rgb, int width, int height) {
+    vector<unsigned char> data(width * height * 3);
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int idx = (y * width + x) * 3;
+            data[idx + 0] = rgb[y][x][0]; // R
+            data[idx + 1] = rgb[y][x][1]; // G
+            data[idx + 2] = rgb[y][x][2]; // B
+        }
+    }
+    // Simpan sebagai PNG (lu bisa ganti ke JPG kalo mau)
+    int success = stbi_write_png(path.c_str(), width, height, 3, data.data(), width * 3);
+
+    if (!success) {
+        cerr << "❌ Gagal menyimpan gambar ke: " << path << endl;
+    } else {
+        cout << "✅ Gambar berhasil disimpan ke: " << path << endl;
+    }
 }
