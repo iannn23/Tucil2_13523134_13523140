16ad5463ce9c031b3f3ff179cfe6d9531753ebeb
Mahesa Fadhillah Andre <167867383+mahesa005@users.noreply.github.com>
2025-04-11 00:00:39 +0700

    feat: add image preprocessing

--- src/ImageIO/ImageIO.cpp
+++ src/ImageIO/ImageIO.cpp
@@ -10,5 +10,25 @@ using namespace std;
 vector<vector<vector<unsigned char>>> loadImage(const string& path, int& width, int& height) {
     int channels;
 
-    unsigned char* data = stbi_load
+    unsigned char* data = stbi_load(path.c_str(), &width, &height, &channels, 3);
+
+    if (!data) {
+        cerr << "Gagal load gambar: " << path << endl;
+        cerr << "Alasan: " << stbi_failure_reason() << endl;
+        exit(1);
+    }
+
+    vector<vector<vector<unsigned char>>> rgb(height, vector<vector<unsigned char>>(width, vector<unsigned char>(3)));
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int idx = (y * width + x) * 3;
+            rgb[y][x][0] = data[idx + 0];    
+            rgb[y][x][1] = data[idx + 1];    
+            rgb[y][x][2] = data[idx + 2];    
+        }
+    }
+    
+    stbi_image_free(data);
+    return rgb;
 }
